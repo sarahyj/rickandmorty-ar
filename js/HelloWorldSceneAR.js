91db899fd22f51670a8ff835d18251cd05527aa9
saljindan <saljindan@3524640.no-reply.drupal.org>
2020-05-12 23:26:44 +0300

    added video on rick scan

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,23 +1,25 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 
 import {
   ViroARScene,
   ViroText,
+  ViroVideo,
   ViroConstants,
-} from 'react-viro';
+  ViroARImageMarker,
+  ViroARTrackingTargets,
+} from "react-viro";
 
 export default class HelloWorldSceneAR extends Component {
-
   constructor() {
     super();
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text: "Initializing AR...",
     };
 
     // bind 'this' to functions
@@ -26,8 +28,25 @@ export default class HelloWorldSceneAR extends Component {
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroARImageMarker target={"targetOne"}>
+          <ViroVideo
+            source={require("./video/rick.mp4")}
+            loop={true}
+            position={[-5, -5, -5]}
+            scale={[2, 2, 0]}
+            volume={1}
+            rotation={[-60, 0, 0]}
+            width={2}
+            height={1}
+          />
+          <ViroText
+            text={this.state.text}
+            scale={[0.5, 0.5, 0.5]}
+            position={[0, 0, -1]}
+            style={styles.helloWorldTextStyle}
+          />
+        </ViroARImageMarker>
       </ViroARScene>
     );
   }
@@ -35,7 +54,7 @@ export default class HelloWorldSceneAR extends Component {
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello World!"
+        text: "Hello Yousef!",
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
@@ -43,13 +62,21 @@ export default class HelloWorldSceneAR extends Component {
   }
 }
 
+ViroARTrackingTargets.createTargets({
+  targetOne: {
+    source: require("./res/rick.jpg"),
+    orientation: "Up",
+    physicalWidth: 0.1, // real world width in meters
+  },
+});
+
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
-    fontFamily: 'Arial',
+    fontFamily: "Arial",
     fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center",
   },
 });
 
